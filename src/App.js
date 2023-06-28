9992ebf7b5eb54093a8da1af38f33cf59df8aaf2
DavidvR2109 <dv357549@gmail.com>
2023-06-28 15:18:25 -0500

    se agrego funcionalidades a foormulario en MY APPNPX

--- src/App.js
+++ src/App.js
@@ -1,18 +1,116 @@
-import React from 'react';
-import useTaskManager from './useTaskManager';
-import TaskList from './TaskList';
-import TaskForm from './TaskForm';
-import './App.css'; 
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const { tasks, createTask, deleteTask, updateTask } = useTaskManager();
+  const [tasks, setTasks] = useState([
+    { id: 1, name: 'Task 1', description: 'Description 1', completed: false },
+    { id: 2, name: 'Task 2', description: 'Description 2', completed: false },
+    { id: 3, name: 'Task 3', description: 'Description 3', completed: true },
+  ]);
+  const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+
+  const completeTask = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, completed: true } : task
+      )
+    );
+  };
+
+  const uncompleteTask = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, completed: false } : task
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this task?');
+    if (confirmDelete) {
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    }
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (taskName.length < 3) {
+      alert('Task name should have at least 3 characters.');
+      return;
+    }
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id: prevTasks.length + 1,
+        name: taskName,
+        description: taskDescription,
+        completed: false,
+      },
+    ]);
+    setTaskName('');
+    setTaskDescription('');
+  };
 
   return (
-    <div className="app-container"> 
-      <h1 className="app-title">Task Manager</h1> 
-      <div className="task-manager-container">
-        <TaskForm createTask={createTask} />
-        <TaskList tasks={tasks} deleteTask={deleteTask} updateTask={updateTask} />
+    <div className="task-manager">
+      <h1>Task Manager</h1>
+
+      <div className="add-task">
+        <h2>Add Task</h2>
+        <form onSubmit={addTask}>
+          <label>
+            Task Name:
+            <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Task Description:
+            <input
+              type="text"
+              value={taskDescription}
+              onChange={(e) => setTaskDescription(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+
+      <div className="pending-tasks">
+        <h2>Pending Tasks</h2>
+        {tasks
+          .filter((task) => !task.completed)
+          .map((task) => (
+            <div key={task.id} className="task">
+              <h3>{task.name}</h3>
+              <p>{task.description}</p>
+              <button className="complete-button" onClick={() => completeTask(task.id)}>
+                Complete Task
+              </button>
+              <button className="delete-button" onClick={() => deleteTask(task.id)}>
+                Delete
+              </button>
+            </div>
+          ))}
+      </div>
+
+      <div className="completed-tasks">
+        <h2>Completed Tasks</h2>
+        {tasks
+          .filter((task) => task.completed)
+          .map((task) => (
+            <div key={task.id} className="task">
+              <h3>{task.name}</h3>
+              <p>{task.description}</p>
+              <button className="uncomplete-button" onClick={() => uncompleteTask(task.id)}>
+                Uncomplete Task
+              </button>
+              <button className="delete-button" onClick={() => deleteTask(task.id)}>
+                Delete
+              </button>
+            </div>
+          ))}
       </div>
     </div>
   );
--- src/TaskForm.js
+++ src/TaskForm.js
@@ -2,28 +2,50 @@ import React, { useState } from 'react';
 
 function TaskForm({ createTask }) {
   const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
 
   const handleTaskNameChange = (event) => {
     setTaskName(event.target.value);
   };
 
+  const handleTaskDescriptionChange = (event) => {
+    setTaskDescription(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newTask = {
-      id: Date.now(),
-      name: taskName,
-    };
-    createTask(newTask);
-    setTaskName('');
+    if (taskName.length >= 3) {
+      const newTask = {
+        id: Date.now(),
+        name: taskName,
+        description: taskDescription,
+      };
+      createTask(newTask);
+      setTaskName('');
+      setTaskDescription('');
+    } else {
+      alert('El nombre de la tarea debe tener al menos 3 caracteres');
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={taskName} onChange={handleTaskNameChange} />
+      <input
+        type="text"
+        value={taskName}
+        onChange={handleTaskNameChange}
+        placeholder="Nombre de la tarea"
+      />
+      <textarea
+        value={taskDescription}
+        onChange={handleTaskDescriptionChange}
+        placeholder="Descripción de la tarea (opcional)"
+      />
       <button type="submit">Crear tarea</button>
     </form>
   );
 }
 
 export default TaskForm;
+
 
